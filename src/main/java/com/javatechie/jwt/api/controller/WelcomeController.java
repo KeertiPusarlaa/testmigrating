2b27dc7c1316da75872a244e266984a26a8fec3f
Keerti Pusarla <Venkatasailakshmikeerti.Pusarla@infinite.com>
2025-10-23 09:53:44 -0500

    Applied coding changes

--- src/main/java/com/javatechie/jwt/api/controller/WelcomeController.java
+++ src/main/java/com/javatechie/jwt/api/controller/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.javatechie.jwt.api.controller;
 
 import com.javatechie.jwt.api.entity.AuthRequest;
+import com.javatechie.jwt.api.repository.UserRepository;
 import com.javatechie.jwt.api.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class WelcomeController {
 
@@ -17,10 +21,19 @@ public class WelcomeController {
     private JwtUtil jwtUtil;
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/")
     public String welcome() {
         return "Welcome to javatechie !!";
+    }
+
+    @GetMapping("/users")
+    public List<UserSummary> users() {
+        return userRepository.findAll().stream()
+                .map(user -> new UserSummary(user.getId(), user.getUserName(), user.getEmail()))
+                .collect(Collectors.toList());
     }
 
     @PostMapping("/authenticate")
@@ -33,5 +46,29 @@ public class WelcomeController {
             throw new Exception("inavalid username/password");
         }
         return jwtUtil.generateToken(authRequest.getUserName());
+    }
+
+    public static class UserSummary {
+        private final int id;
+        private final String userName;
+        private final String email;
+
+        public UserSummary(int id, String userName, String email) {
+            this.id = id;
+            this.userName = userName;
+            this.email = email;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
     }
 }
