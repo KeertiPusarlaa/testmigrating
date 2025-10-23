c3ed486db3a7123220052c6bfe717120a2e85551
Keerti Pusarla <Venkatasailakshmikeerti.Pusarla@infinite.com>
2025-10-23 13:35:04 -0500

    Applied coding changes

--- src/main/java/com/javatechie/jwt/api/config/SecurityConfig.java
+++ src/main/java/com/javatechie/jwt/api/config/SecurityConfig.java
@@ -43,10 +43,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate", "/login")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
--- src/main/java/com/javatechie/jwt/api/controller/WelcomeController.java
+++ src/main/java/com/javatechie/jwt/api/controller/WelcomeController.java
@@ -1,14 +1,23 @@
 package com.javatechie.jwt.api.controller;
 
 import com.javatechie.jwt.api.entity.AuthRequest;
+import com.javatechie.jwt.api.entity.User;
+import com.javatechie.jwt.api.repository.UserRepository;
 import com.javatechie.jwt.api.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WelcomeController {
@@ -17,6 +26,8 @@ public class WelcomeController {
     private JwtUtil jwtUtil;
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/")
     public String welcome() {
@@ -24,14 +35,34 @@ public class WelcomeController {
     }
 
     @PostMapping("/authenticate")
-    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+    public String generateToken(@RequestBody AuthRequest authRequest) {
+        authenticate(authRequest);
+        return jwtUtil.generateToken(authRequest.getUserName());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody AuthRequest authRequest) {
+        authenticate(authRequest);
+
+        User user = userRepository.findByUserName(authRequest.getUserName());
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist");
+        }
+
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("token", jwtUtil.generateToken(user.getUserName()));
+        responseBody.put("userName", user.getUserName());
+        responseBody.put("email", user.getEmail());
+        return ResponseEntity.ok(responseBody);
+    }
+
+    private void authenticate(AuthRequest authRequest) {
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword())
             );
-        } catch (Exception ex) {
-            throw new Exception("inavalid username/password");
+        } catch (AuthenticationException ex) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid username/password");
         }
-        return jwtUtil.generateToken(authRequest.getUserName());
     }
 }
