8975ec17a250cf6b41acae76d0a5b4da0ba4d193
Keerti Pusarla <Venkatasailakshmikeerti.Pusarla@infinite.com>
2025-10-24 10:09:26 -0500

    Applied coding changes

--- src/main/java/com/javatechie/jwt/api/controller/WelcomeController.java
+++ src/main/java/com/javatechie/jwt/api/controller/WelcomeController.java
@@ -1,14 +1,19 @@
 package com.javatechie.jwt.api.controller;
 
 import com.javatechie.jwt.api.entity.AuthRequest;
+import com.javatechie.jwt.api.entity.User;
+import com.javatechie.jwt.api.repository.UserRepository;
 import com.javatechie.jwt.api.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class WelcomeController {
@@ -17,10 +22,18 @@ public class WelcomeController {
     private JwtUtil jwtUtil;
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository repository;
 
     @GetMapping("/")
     public String welcome() {
         return "Welcome to javatechie !!";
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> users = repository.findAll();
+        return ResponseEntity.ok(users);
     }
 
     @PostMapping("/authenticate")
